72a5d8b1e826fb5c660dabf8f099d38671402b32
Jamie Pitts <jpitts@users.noreply.github.com>
2019-02-16 17:15:40 -0800

    Updated the client-side renderer to reflect changes made for the NuCypher test.

--- renderer.js
+++ renderer.js
@@ -3,25 +3,43 @@ let client = new zerorpc.Client()
 
 client.connect("tcp://127.0.0.1:4242")
 
-client.invoke("echo", "server ready", (error, res) => {
-  if(error || res !== 'server ready') {
+client.invoke("echo", "zerorpc server ready", (error, res) => {
+  if(error || res !== 'zerorpc server ready') {
     console.error(error)
   } else {
-    console.log("server is ready")
+    console.log("zerorpc server is ready")
   }
 })
 
 let formula = document.querySelector('#formula')
-let result = document.querySelector('#result')
+let test = document.querySelector('#run-test')
+let status = document.querySelector('#status')
+let aliceResult = document.querySelector('#alice-result')
+let bobResult = document.querySelector('#bob-result')
 
-formula.addEventListener('input', () => {
-  client.invoke("calc", formula.value, (error, res) => {
+test.addEventListener('click', () => {
+
+  status.textContent = '';
+  bobResult.textContent = '';
+  aliceResult.textContent = '';
+
+  client.invoke("calc", formula.value, (error, resContent) => {
     if(error) {
       console.error(error)
     } else {
-      result.textContent = res
+      resJson = JSON.parse(resContent)
+      status.textContent = resJson.success
+      aliceResult.textContent = resJson.alice_cleartext
+      bobResult.textContent = resJson.bob_cleartext
     }
   })
 })
 
-formula.dispatchEvent(new Event('input'))
+// start by running the process
+
+formula.textContent = 'Enter some sample text in order to perform a pyUmbral test.';
+
+setTimeout(
+  () => { 
+    test.dispatchEvent(new Event('click'))
+  }, 100)
